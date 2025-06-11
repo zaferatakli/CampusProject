7e9de680611279e9687e34d5806805e27774eb37
zaferatakli <zatakli@gmail.com>
2025-06-12 05:41:00 +1000

    Robot class added the project

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -7,12 +7,16 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 import java.time.Duration;
 
 public class ReusableMethods {
     public WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(ConfigReader.getIntProperty("explicitWait")));
     public Actions action = new Actions(GWD.getDriver());
+    public Robot robot;
 
     public void myClick(WebElement element) {
         wait.until(ExpectedConditions.elementToBeClickable(element));
@@ -68,6 +72,28 @@ public class ReusableMethods {
             Thread.sleep(sn * 1000L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public void pressTab() {
+        try {
+            robot = new Robot();
+            robot.delay(500);
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pressEnter() {
+        try {
+            robot = new Robot();
+            robot.delay(500);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (AWTException e) {
+            e.printStackTrace();
         }
     }
 }
