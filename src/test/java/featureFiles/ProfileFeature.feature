e0307bc3d0fbda434365540cf0ae6134872bd951
theeren1905 <erenozzbbk@gmail.com>
2025-06-16 23:36:23 +0300

    ProfileFeature.feature created

---
+++ src/test/java/featureFiles/ProfileFeature.feature
@@ -0,0 +1,8 @@
+Feature: Profile feature
+
+  Background:
+    Given Navigate to Campus
+    When The user types username and password and click login button
+    Then The user verifies login successfully
+  Scenario:Uploading your profile photo
+  Given Clicks on the profile photo in the top right corner
---
+++ src/test/java/stepDefinitions/ProfileFeature.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import pages.DialogContent;
+import pages.TopNav;
+
+public class ProfileFeature {
+    TopNav tn = new TopNav();
+    DialogContent dc = new DialogContent();
+
+    @Given("Clicks on the profile photo in the top right corner")
+    public void clicksOnTheProfilePhotoInTheTopRightCorner() {
+
+
+
+
+    }
+
+    }
